41f8113b227284c57ce20ab7a7b5ef423372bdc3
Nilutpal-Gogoi <nilutpal.ee@gmail.com>
2024-02-23 14:53:57 +0530

    second commit

---
+++ src/main/java/com/learning/demo/junitandmockito/JunitandmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demo.junitandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitandmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitandmockitoApplication.class, args);
+	}
+
+}
